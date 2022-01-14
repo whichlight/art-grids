b7ce0c96c7ddca0cd880082a8e4df5c117af938c
Kawandeep Virdee <kawandeepvirdee@Kawandeeps-MacBook-Pro.local>
2022-01-14 17:14:20 -0500

    deploying locally

--- contracts/Grid.sol
+++ contracts/Grid.sol
@@ -7,15 +7,20 @@ import "hardhat/console.sol";
 /* 
 these are standard/existing contracts. can replace them with links to ones on openzeppelin 
 https://docs.openzeppelin.com/contracts/2.x/api/token/erc721
-https://docs.openzeppelin.com/contracts/2.x/api/math
+https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol
 https://docs.openzeppelin.com/contracts/4.x/access-control
 
-*/
-import "./erc721.sol";
-import "./safemath.sol";
-import "./ownable.sol";
 
-contract GridFactory {
+import "./erc721.sol";
+import "./ownable.sol";
+*/
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+
+contract Grid{
     using SafeMath for uint256;
 
     uint256 dnaDigits = 12; //64*64 or 2^11
@@ -32,7 +37,8 @@ contract GridFactory {
     mapping(uint256 => address) public gridToOwner;
 
     function _createGrid(uint256 _dna) internal {
-        uint256 id = grids.push(Grid(_dna)) - 1;
+        grids.push(Grid(_dna));
+        uint256 id = grids.length - 1; 
         idToDna[id] = _dna;
 
         //should we do this?
@@ -40,15 +46,20 @@ contract GridFactory {
     }
 
     // creating a grid based on a string
-    function _generateGrid(string _str) internal returns (uint256) {
+    function _generateGrid(string memory _str) internal {
         uint256 val = uint256(keccak256(abi.encodePacked(_str)));
         _createGrid(val % dnaModulus);
     }
 
     //creating a grid based on someone's public key
+
+    /*
+    this isn't working bc of the address and string conversion 
     function createGridAddress() public {
-        uint256 gridDna = _generateGrid(msg.sender);
+        //need to convert address to string. running encodepacked twice, is this okay? 
+        uint256 gridDna = _generateGrid(abi.encodePacked(msg.sender));
     }
+    */ 
 
     //retrieve a grid given the id
     function getGrid(uint256 id) public view returns (uint256) {
@@ -57,9 +68,12 @@ contract GridFactory {
 
     //retrieve a list of existing grid id's. this is just the index up to the length. 
     // do we want another way to represent id? 
-    function getGridIds() public view returns (uint256[]) {
+    // is the return calldata or memory 
+    function getGridIds() public view returns (uint256[] memory) {
         uint256 len = grids.length;
         uint256[] memory result = new uint256[](len);
+        uint counter = 0;
+
         for (uint256 i = 0; i < grids.length; i++) {
                 result[counter] = i;
                 counter++;
---
+++ scripts/deploy.js
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const Grid = await hre.ethers.getContractFactory("Grid");
+  const grid = await Grid.deploy();
+
+  await grid.deployed();
+
+  console.log("Grid deployed to:", grid.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
