7dd408387ff0e33c9205ba26648ba7a03d1ab989
whichlight <whichlight@gmail.com>
2022-01-15 10:59:04 -0500

    adding nft stuff

--- art/art.js
+++ art/art.js
@@ -20,8 +20,8 @@ async function printblock(){
 printblock();
 
 //get a grid 
-
-const gridAddress = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
+//how to update this without having to do it on each deploy? 
+const gridAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 const gridAbi = [
     //get number of grids
--- contracts/Grid.sol
+++ contracts/Grid.sol
@@ -5,18 +5,26 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 
-contract GridFactory{
+contract GridFactory is ERC721{
     using SafeMath for uint256;
+    using Counters for Counters.Counter;
 
+
+    Counters.Counter private _tokenIds;
     uint256 dnaDigits = 12; //64*64 or 2^11
     uint256 dnaModulus = 10**dnaDigits;
 
-    //create a few grids
-    constructor()  {
+    // token info 
+    string token_name = "grid";
+    string token_symbol = "GRID";
+
+    //create a few grids, use NFT to create 
+    constructor() ERC721(token_name, token_symbol)  {
         _generateGrid("yay");
         _generateGrid("cool");
         _generateGrid("fun");
@@ -32,15 +40,7 @@ contract GridFactory{
     mapping(uint256 => uint256) public idToDna;
     mapping(uint256 => address) public gridToOwner;
 
-    function _createGrid(uint256 _dna) internal {
-        grids.push(Grid(_dna));
-        uint256 id = grids.length - 1; 
-        idToDna[id] = _dna;
-
-        //should we do this?
-        gridToOwner[id] = msg.sender;
-    }
-
+  
     // creating a grid based on a string
     function _generateGrid(string memory _str) internal {
         uint256 val = uint256(keccak256(abi.encodePacked(_str)));
@@ -55,6 +55,17 @@ contract GridFactory{
         uint256 gridDna = _generateGrid(abi.encodePacked(msg.sender));
     }
     */ 
+
+
+// creates a grid and mints it 
+      function _createGrid(uint256 _dna) internal {
+        uint256 id;
+        id = mint(msg.sender);
+        grids.push(Grid(_dna));
+        idToDna[id] = _dna;
+        gridToOwner[id] = msg.sender;
+
+    }
 
     //retrieve a grid given the id
     function getGrid(uint256 id) public view returns (uint256) {
@@ -76,5 +87,15 @@ contract GridFactory{
     
         }
         return result;
+    }
+
+// allow anyone to mint, removed tokenuri
+    function mint(address recipient) public returns (uint256)
+    {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+      //  _setTokenURI(newItemId, tokenURI); // might need ERC721Full
+        return newItemId;
     }
 }
